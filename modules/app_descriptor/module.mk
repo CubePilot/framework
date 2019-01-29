50082e859ee91f1b79ff66c5afd4c418a513fbac
Jonathan Challinger <mr.challinger@gmail.com>
2019-01-28 16:43:57 -0800

    support building combined bootloader+app binary

--- modules/app_descriptor/module.mk
+++ modules/app_descriptor/module.mk
@@ -4,3 +4,11 @@ APP_DESCRIPTOR_MODULE_DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST)))
 POST_MAKE_ALL_RULE_HOOK: $(BUILDDIR)/$(PROJECT)-crc.bin
 $(BUILDDIR)/$(PROJECT)-crc.bin: $(BUILDDIR)/$(PROJECT).bin
 	python $(APP_DESCRIPTOR_MODULE_DIR)/crc_binary.py $< $@
+
+ifneq (,$(wildcard $(BOARD_DIR)/bootloader.bin))
+POST_MAKE_ALL_RULE_HOOK: $(BUILDDIR)/$(PROJECT)-combined.bin
+$(BUILDDIR)/$(PROJECT)-combined.bin: $(BUILDDIR)/$(PROJECT)-crc.bin
+	dd if=/dev/zero bs=$(BOOTLOADER_SIZE) count=1 | tr "\000" "\377" > $@
+	dd if=$(BOARD_DIR)/bootloader.bin of=$@ conv=notrunc
+	dd oflag=seek_bytes if=$(BUILDDIR)/$(PROJECT)-crc.bin of=$@ seek=$(BOOTLOADER_SIZE)
+endif
