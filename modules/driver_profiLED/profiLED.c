5c3a0ced93c7644cfae4aa1ac5a4a17ea3cb6a5a
Jonathan Challinger <mr.challinger@gmail.com>
2019-04-02 11:57:36 -0700

    profiled support without spi

--- modules/driver_profiLED/profiLED.c
+++ modules/driver_profiLED/profiLED.c
@@ -1,4 +1,5 @@
 #include <ch.h>
+#include <hal.h>
 #include <string.h>
 #include "profiLED.h"
 #include <common/helpers.h>
@@ -12,28 +13,12 @@
 #define PROFILED_WORKER_THREAD_STACK_SIZE 256
 #define PROFILED_WORKER_MAILBOX_DEPTH 2
 
-#ifndef PROFILED_BLOCKING
-static struct profiLED_instance_s* worker_mailbox_buf[PROFILED_WORKER_MAILBOX_DEPTH];
-MAILBOX_DECL(worker_mailbox, worker_mailbox_buf, PROFILED_WORKER_MAILBOX_DEPTH);
-static thread_t* worker_thread;
-
-static THD_WORKING_AREA(waProfiLEDThread, PROFILED_WORKER_THREAD_STACK_SIZE);
-static THD_FUNCTION(ProfiLEDThread, arg);
-#endif
-
 static void _profiled_update(struct profiLED_instance_s* instance);
 
 void profiLED_init(struct profiLED_instance_s* instance, uint8_t spi_bus_idx, uint32_t spi_sel_line, bool sel_active_high, uint32_t num_leds) {
     if (!instance) {
         return;
     }
-
-#ifndef PROFILED_BLOCKING
-    if (!worker_thread) {
-        worker_thread = chThdCreateStatic(waProfiLEDThread, sizeof(waProfiLEDThread), LOWPRIO, ProfiLEDThread, NULL);
-        if (!worker_thread) goto fail;
-    }
-#endif
 
     size_t colors_size = sizeof(struct profiLED_gen_color_s) * num_leds;
 
@@ -46,6 +31,7 @@ void profiLED_init(struct profiLED_instance_s* instance, uint8_t spi_bus_idx, ui
     }
 
     instance->num_leds = num_leds;
+    instance->use_spi = true;
 
     profiLED_update(instance);
     return;
@@ -54,13 +40,32 @@ fail:
     instance->colors = NULL;
 }
 
+void profiLED_bitbang_send(struct profiLED_instance_s* instance, size_t buf_len, uint8_t* txbuf) {
+    for (size_t i=0; i<buf_len; i++) {
+        for (int8_t bit = 7; bit >= 0; bit++) {
+            palSetLine(instance->sclk_line);
+            palWriteLine(instance->mosi_line, (txbuf[i]>>bit)&1);
+            palClearLine(instance->sclk_line);
+        }
+    }
+}
+
 void profiLED_update(struct profiLED_instance_s* instance) {
-#ifdef PROFILED_BLOCKING
-    _profiled_update(instance);
-#else
-    // signal the worker thread to update this LED instance
-    chMBPost(&worker_mailbox, (msg_t)instance, TIME_IMMEDIATE);
-#endif
+    if (!instance) {
+        return;
+    }
+
+    uint8_t txbuf[PROFILED_OUTPUT_BUFFER_SIZE];
+    uint32_t buf_len = profiLED_gen_write_buf(instance->num_leds, instance->colors, txbuf, sizeof(txbuf));
+    if (buf_len == 0) {
+        return;
+    }
+
+    if (use_spi) {
+        spi_device_send(&(instance->dev), buf_len, txbuf);
+    } else {
+        profiLED_bitbang_send(instance, buf_len, txbuf);
+    }
 }
 
 void profiLED_set_color_rgb(struct profiLED_instance_s* instance, uint32_t idx, uint8_t r, uint8_t g, uint8_t b) {
@@ -74,29 +79,3 @@ void profiLED_set_color_rgb(struct profiLED_instance_s* instance, uint32_t idx, 
 void profiLED_set_color_hex(struct profiLED_instance_s* instance, uint32_t idx, uint32_t color) {
     profiLED_set_color_rgb(instance, idx, (uint8_t)(color>>16), (uint8_t)(color>>8), (uint8_t)color);
 }
-
-static void _profiled_update(struct profiLED_instance_s* instance) {
-    if (!instance) {
-        return;
-    }
-
-    uint8_t txbuf[PROFILED_OUTPUT_BUFFER_SIZE];
-    uint32_t buf_len = profiLED_gen_write_buf(instance->num_leds, instance->colors, txbuf, sizeof(txbuf));
-    if (buf_len == 0) {
-        return;
-    }
-
-    spi_device_send(&(instance->dev), buf_len, txbuf);
-}
-
-#ifndef PROFILED_BLOCKING
-static THD_FUNCTION(ProfiLEDThread, arg) {
-    (void)arg;
-
-    while(true) {
-        msg_t instance;
-        chMBFetch(&worker_mailbox, &instance, TIME_INFINITE);
-        _profiled_update((struct profiLED_instance_s*)instance);
-    }
-}
-#endif
--- modules/driver_profiLED/profiLED.h
+++ modules/driver_profiLED/profiLED.h
@@ -2,14 +2,19 @@
 
 #include <modules/spi_device/spi_device.h>
 #include "profiLED_gen.h"
+#include <stdbool.h>
 
 struct profiLED_instance_s {
+    bool use_spi;
     struct spi_device_s dev;
+    uint32_t sclk_line;
+    uint32_t mosi_line;
     uint32_t num_leds;
     struct profiLED_gen_color_s* colors;
 };
 
 void profiLED_init(struct profiLED_instance_s* instance, uint8_t spi_bus_idx, uint32_t spi_sel_line, bool sel_active_high, uint32_t num_leds);
+void profiLED_init_nospi(struct profiLED_instance_s* instance, uint32_t sclk_line, uint32_t mosi_line, uint32_t num_leds);
 void profiLED_update(struct profiLED_instance_s* instance);
 void profiLED_set_color_rgb(struct profiLED_instance_s* instance, uint32_t idx, uint8_t r, uint8_t g, uint8_t b);
 void profiLED_set_color_hex(struct profiLED_instance_s* instance, uint32_t idx, uint32_t color);
