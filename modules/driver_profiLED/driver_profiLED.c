d24542a233babb997037075255037e06bb921c57
Jonathan Challinger <mr.challinger@gmail.com>
2019-04-26 13:23:56 -0700

    driver_profiLED: change to callback-based operation

--- modules/driver_profiLED/driver_profiLED.c
+++ modules/driver_profiLED/driver_profiLED.c
@@ -3,59 +3,125 @@
 #include "driver_profiLED.h"
 #include <common/helpers.h>
 #include <app_config.h>
+#include <hal.h>
 
-static void _profiled_update(struct profiLED_instance_s* instance);
+#define PROFILED_SEND_BUF_SIZE 32
 
-void profiLED_init(struct profiLED_instance_s* instance, uint8_t spi_bus_idx, uint32_t spi_sel_line, bool sel_active_high, uint32_t num_leds) {
-    if (!instance) {
-        return;
+void profiLED_output_gpio(uint32_t sclk_line, uint32_t mosi_line, uint32_t num_leds, profiLED_color_func color_func, void* ctx) {
+    const uint32_t min_bits = num_leds*25+50;
+    const uint8_t num_leading_zeros = 8-min_bits%8 + 50;
+    const uint32_t output_stream_byte_length = (min_bits+7)/8;
+
+    uint32_t curr_led_idx = 0;
+
+    union {
+        struct profiLED_color_s struct_val;
+        uint8_t bytes_val[3];
+    } curr_led_color;
+
+    curr_led_color.struct_val = color_func(curr_led_idx, ctx);
+
+    for (uint32_t i=0; i<output_stream_byte_length; i++) {
+        uint8_t byte = 0;
+        for (uint8_t bit = 0; bit < 8; bit++) {
+            uint32_t out_bit_idx = i*8+bit;
+            uint8_t bit_val;
+            if (out_bit_idx < num_leading_zeros) {
+                bit_val = 0;
+            } else if ((out_bit_idx-num_leading_zeros) % 25 == 0) {
+                bit_val = 1;
+            } else {
+                uint32_t in_bit_idx = out_bit_idx - num_leading_zeros - (out_bit_idx - num_leading_zeros)/25;
+                uint32_t in_led_idx = in_bit_idx/24;
+
+                if (curr_led_idx != in_led_idx) {
+                    curr_led_idx = in_led_idx;
+                    curr_led_color.struct_val = color_func(curr_led_idx, ctx);
+                }
+
+                bit_val = (curr_led_color.bytes_val[(in_bit_idx%24)/8] >> (8-in_bit_idx%8)) & 1;
+            }
+
+            palClearLine(sclk_line);
+            // TODO insert delay
+            palWriteLine(mosi_line, bit_val);
+            // TODO insert delay
+            palSetLine(sclk_line);
+            // TODO insert delay
+        }
     }
-
-    size_t colors_size = sizeof(struct profiLED_gen_color_s) * num_leds;
-
-    instance->colors = chCoreAlloc(colors_size);
-    if (!instance->colors) goto fail;
-    memset(instance->colors, 0, colors_size);
-
-    if (!spi_device_init(&(instance->dev), spi_bus_idx, spi_sel_line, 30000000, 8, (sel_active_high?SPI_DEVICE_FLAG_SELPOL:0))) {
-        goto fail;
-    }
-
-    instance->num_leds = num_leds;
-
-    profiLED_update(instance);
-    return;
-
-fail:
-    instance->colors = NULL;
 }
 
-void profiLED_update(struct profiLED_instance_s* instance) {
-    _profiled_update(instance);
+#ifdef MODULE_SPI_DEVICE_ENABLED
+bool profiLED_spi_dev_init(struct spi_device_s* dev, uint8_t spi_bus_idx, uint32_t spi_sel_line, bool sel_active_high, uint32_t speed_hz) {
+    return spi_device_init(dev, spi_bus_idx, spi_sel_line, speed_hz, 8, (sel_active_high?SPI_DEVICE_FLAG_SELPOL:0));
 }
 
-void profiLED_set_color_rgb(struct profiLED_instance_s* instance, uint32_t idx, uint8_t r, uint8_t g, uint8_t b) {
-    if (!instance || !instance->colors) {
-        return;
-    }
+void profiLED_output_spi(struct spi_device_s* dev, uint32_t num_leds, profiLED_color_func color_func, void* ctx) {
+    const uint32_t min_bits = num_leds*25+50;
+    const uint8_t num_leading_zeros = 8-min_bits%8 + 50;
+    const uint32_t output_stream_byte_length = (min_bits+7)/8;
 
-    profiLED_gen_make_brg_color_rgb(r, g, b, &instance->colors[idx]);
+    uint32_t curr_led_idx = 0;
+
+    union {
+        struct profiLED_color_s struct_val;
+        uint8_t bytes_val[3];
+    } curr_led_color;
+
+    curr_led_color.struct_val = color_func(curr_led_idx, ctx);
+
+    uint8_t send_buf[PROFILED_SEND_BUF_SIZE];
+    uint8_t send_buf_len = 0;
+
+    for (uint32_t i=0; i<output_stream_byte_length; i++) {
+        uint8_t byte = 0;
+        for (uint8_t bit = 0; bit < 8; bit++) {
+            uint32_t out_bit_idx = i*8+bit;
+            uint8_t bit_val;
+            if (out_bit_idx < num_leading_zeros) {
+                bit_val = 0;
+            } else if ((out_bit_idx-num_leading_zeros) % 25 == 0) {
+                bit_val = 1;
+            } else {
+                uint32_t in_bit_idx = out_bit_idx - num_leading_zeros - (out_bit_idx - num_leading_zeros)/25;
+                uint32_t in_led_idx = in_bit_idx/24;
+
+                if (curr_led_idx != in_led_idx) {
+                    curr_led_idx = in_led_idx;
+                    curr_led_color.struct_val = color_func(curr_led_idx, ctx);
+                }
+
+                bit_val = (curr_led_color.bytes_val[(in_bit_idx%24)/8] >> (8-in_bit_idx%8)) & 1;
+            }
+
+            byte |= bit_val << (7-bit);
+        }
+        send_buf[send_buf_len++] = byte;
+        if (send_buf_len >= PROFILED_SEND_BUF_SIZE) {
+            spi_device_send(dev, send_buf_len, send_buf);
+            send_buf_len = 0;
+        }
+    }
+    if (send_buf_len > 0) {
+        spi_device_send(dev, send_buf_len, send_buf);
+        send_buf_len = 0;
+    }
+}
+#endif
+
+struct profiLED_color_s profiLED_make_color_from_hex(uint32_t color) {
+    struct profiLED_color_s ret;
+    ret.r = (uint8_t)(color>>16);
+    ret.g = (uint8_t)(color>>8);
+    ret.b = (uint8_t)color;
+    return ret;
 }
 
-void profiLED_set_color_hex(struct profiLED_instance_s* instance, uint32_t idx, uint32_t color) {
-    profiLED_set_color_rgb(instance, idx, (uint8_t)(color>>16), (uint8_t)(color>>8), (uint8_t)color);
-}
-
-static void _profiled_update(struct profiLED_instance_s* instance) {
-    if (!instance) {
-        return;
-    }
-
-    uint8_t txbuf[PROFILED_GEN_BUF_SIZE(instance->num_leds)];
-    uint32_t buf_len = profiLED_gen_write_buf(instance->num_leds, instance->colors, txbuf, sizeof(txbuf));
-    if (buf_len == 0) {
-        return;
-    }
-
-    spi_device_send(&(instance->dev), buf_len, txbuf);
+struct profiLED_color_s profiLED_make_color_from_rgb_float(float r, float g, float b) {
+    struct profiLED_color_s ret;
+    ret.r = r < 0 ? 0 : (r > 1 ? 255 : r*255);
+    ret.g = g < 0 ? 0 : (g > 1 ? 255 : g*255);
+    ret.b = b < 0 ? 0 : (b > 1 ? 255 : b*255);
+    return ret;
 }
--- modules/driver_profiLED/driver_profiLED.h
+++ modules/driver_profiLED/driver_profiLED.h
@@ -1,15 +1,24 @@
 #pragma once
 
+#ifdef MODULE_SPI_DEVICE_ENABLED
 #include <modules/spi_device/spi_device.h>
-#include "profiLED_gen.h"
+#endif
 
-struct profiLED_instance_s {
-    struct spi_device_s dev;
-    uint32_t num_leds;
-    struct profiLED_gen_color_s* colors;
+#include <stdint.h>
+
+struct profiLED_color_s {
+    uint8_t b;
+    uint8_t r;
+    uint8_t g;
 };
 
-void profiLED_init(struct profiLED_instance_s* instance, uint8_t spi_bus_idx, uint32_t spi_sel_line, bool sel_active_high, uint32_t num_leds);
-void profiLED_update(struct profiLED_instance_s* instance);
-void profiLED_set_color_rgb(struct profiLED_instance_s* instance, uint32_t idx, uint8_t r, uint8_t g, uint8_t b);
-void profiLED_set_color_hex(struct profiLED_instance_s* instance, uint32_t idx, uint32_t color);
+typedef struct profiLED_color_s (*profiLED_color_func)(uint32_t led_idx, void* ctx);
+void profiLED_output(uint32_t sclk_line, uint32_t mosi_line, uint32_t num_leds, profiLED_color_func color_func, void* ctx);
+
+#ifdef MODULE_SPI_DEVICE_ENABLED
+bool profiLED_spi_dev_init(struct spi_device_s* dev, uint8_t spi_bus_idx, uint32_t spi_sel_line, bool sel_active_high, uint32_t speed_hz);
+void profiLED_output_spi(struct spi_device_s* dev, uint32_t num_leds, profiLED_color_func color_func, void* ctx);
+#endif
+
+struct profiLED_color_s profiLED_make_color_from_hex(uint32_t hex_val);
+struct profiLED_color_s profiLED_make_color_from_rgb_float(float r, float g, float b);
