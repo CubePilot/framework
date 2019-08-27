02b989b336007e44567a1a57fd97b073b22b05df
Jonathan Challinger <mr.challinger@gmail.com>
2019-08-27 11:22:43 -0700

    bootloader: add usb slcan

--- bootloader/include/framework_conf.h
+++ bootloader/include/framework_conf.h
@@ -13,10 +13,11 @@
 #define UAVCAN_BEGINFIRMWAREUPDATE_SERVER_WORKER_THREAD lpwork_thread
 #define UAVCAN_ALLOCATEE_WORKER_THREAD                  lpwork_thread
 #define BOOTLOADER_APP_THREAD                           lpwork_thread
+#define USB_SLCAN_WORKER_THREAD                         can_thread
 
-#define CAN_TRX_WORKER_THREAD                           lpwork_thread
-#define CAN_EXPIRE_WORKER_THREAD                        lpwork_thread
-#define UAVCAN_RX_WORKER_THREAD                         lpwork_thread
+#define CAN_TRX_WORKER_THREAD                           can_thread
+#define CAN_EXPIRE_WORKER_THREAD                        can_thread
+#define UAVCAN_RX_WORKER_THREAD                         can_thread
 
 //
 // Configure topic groups
@@ -32,3 +33,5 @@
 #define CH_DBG_ENABLE_CHECKS                FALSE
 #define CH_DBG_ENABLE_ASSERTS               FALSE
 #define CH_DBG_ENABLE_STACK_CHECK           FALSE
+
+#define CAN_TX_QUEUE_LEN 256
--- bootloader/src/setup.c
+++ bootloader/src/setup.c
@@ -1,6 +1,7 @@
 #include <modules/worker_thread/worker_thread.h>
 #include <modules/pubsub/pubsub.h>
 
-PUBSUB_TOPIC_GROUP_CREATE(default_topic_group, 2048)
+PUBSUB_TOPIC_GROUP_CREATE(default_topic_group, 16384)
 
-WORKER_THREAD_TAKEOVER_MAIN(lpwork_thread, HIGHPRIO)
+WORKER_THREAD_TAKEOVER_MAIN(lpwork_thread, LOWPRIO+1)
+WORKER_THREAD_SPAWN(can_thread, LOWPRIO+2, 2048)
