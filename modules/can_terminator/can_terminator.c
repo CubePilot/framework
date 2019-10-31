04fc540e3c857d6a065af7fa91176dd9f8859f6f
Jonathan Challinger <mr.challinger@gmail.com>
2019-10-31 13:07:49 +0800

    can_terminator: add support for 2 CAN terminators

--- modules/can_terminator/can_terminator.c
+++ modules/can_terminator/can_terminator.c
@@ -2,8 +2,20 @@
 #include <common/ctor.h>
 #include <hal.h>
 
+#ifdef BOARD_PAL_LINE_CAN1_TERMINATE
 PARAM_DEFINE_BOOL_PARAM_STATIC(can1_terminator, "CAN1_TERMINATOR", false)
+#endif
+
+#ifdef BOARD_PAL_LINE_CAN2_TERMINATE
+PARAM_DEFINE_BOOL_PARAM_STATIC(can2_terminator, "CAN2_TERMINATOR", false)
+#endif
 
 RUN_ON(INIT_END) {
+#ifdef BOARD_PAL_LINE_CAN1_TERMINATE
     palWriteLine(BOARD_PAL_LINE_CAN1_TERMINATE, can1_terminator);
+#endif
+
+#ifdef BOARD_PAL_LINE_CAN2_TERMINATE
+    palWriteLine(BOARD_PAL_LINE_CAN2_TERMINATE, can2_terminator);
+#endif
 }
