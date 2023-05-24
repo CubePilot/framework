45b22abb1ba5781931806eccb971243b59f5b28d
Jonathan Challinger <mr.challinger@gmail.com>
2023-05-24 13:46:48 -0400

    remove #error when building with gcc other than 6.3.1

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -27,10 +27,6 @@
 #include <com.hex.file.FileStreamChunk.h>
 #endif // BOOTLOADER_SUPPORT_BROADCAST_UPDATE
 
-#if __GNUC__ != 6 || __GNUC_MINOR__ != 3 || __GNUC_PATCHLEVEL__ != 1
-#error Please use arm-none-eabi-gcc 6.3.1.
-#endif
-
 #ifndef BOOTLOADER_APP_THREAD
 #error Please define BOOTLOADER_APP_THREAD in worker_threads_conf.h.
 #endif
