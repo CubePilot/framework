cd1912d109e32682fc13190aac6c8911814f2c70
Jonathan Challinger <mr.challinger@gmail.com>
2023-07-07 22:30:45 -0400

    fix platform_stm32f767

--- modules/platform_stm32f767/platform_stm32f767.h
+++ modules/platform_stm32f767/platform_stm32f767.h
@@ -1,8 +1,9 @@
 #pragma once
 
+#define STM32F767xx
+
 #if !defined(_FROM_ASM_)
 #include <stdint.h>
-#define STM32F767xx
 
 #define BOARD_PARAM1_FLASH_SIZE ((size_t)&_param1_flash_sec_end - (size_t)&_param1_flash_sec)
 #define BOARD_PARAM2_FLASH_SIZE ((size_t)&_param2_flash_sec_end - (size_t)&_param2_flash_sec)
