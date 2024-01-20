e42399ce5ed146405bc52ad2e171692a2903fcdd
Jonathan Challinger <mr.challinger@gmail.com>
2024-01-20 15:19:41 -0500

    add platform_stm32f405

---
+++ modules/platform_stm32f405/platform_stm32f405.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#define STM32F405xx
+
+#if !defined(_FROM_ASM_)
+#include <stdint.h>
+
+
+#define BOARD_PARAM1_FLASH_SIZE ((size_t)&_param1_flash_sec_end - (size_t)&_param1_flash_sec)
+#define BOARD_PARAM2_FLASH_SIZE ((size_t)&_param2_flash_sec_end - (size_t)&_param2_flash_sec)
+
+#define BOARD_PARAM1_ADDR (&_param1_flash_sec)
+#define BOARD_PARAM2_ADDR (&_param2_flash_sec)
+
+extern uint8_t _param1_flash_sec;
+extern uint8_t _param1_flash_sec_end;
+extern uint8_t _param2_flash_sec;
+extern uint8_t _param2_flash_sec_end;
+
+void board_get_unique_id(uint8_t* buf, uint8_t len);
+
+void boardInit(void);
+#endif /* _FROM_ASM_ */
